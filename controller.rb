077a3c067c1b6cc46d8f140c1ee36f8494590a8f
Becky <rebeccajoynielsen@gmail.come>
2019-03-23 12:43:58 +0000

    add show to controller, can view individual transaction details

--- controller.rb
+++ controller.rb
@@ -7,7 +7,12 @@ require_relative('./models/tag.rb')
 require_relative('./models/transaction.rb')
 also_reload('./models/*')
 
-get '/spending-tracker/' do
+get '/spending-tracker/' do #index
   @transactions = Transaction.all
   erb(:index)
+end
+
+get '/spending-tracker/:id' do #show
+  @transaction = Transaction.find(params[:id])
+  erb(:show)
 end
