a5f9e7f3b0deec4a43c31ef154a2600fdd28b9e8
Becky <rebeccajoynielsen@gmail.come>
2019-03-24 15:52:27 +0000

    add tags index to view all tags

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/contrib/all')
 
 require_relative('./controllers/transactions_controller')
 require_relative('./controllers/merchants_controller')
+require_relative('./controllers/tags_controller')
 
 get '/spending-tracker/' do
   erb(:index)
---
+++ controllers/tags_controller.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/contrib/all')
+#
+# require_relative('../models/account.rb')
+# require_relative('../models/merchant.rb')
+require_relative('../models/tag.rb')
+# require_relative('../models/transaction.rb')
+also_reload('../models/*')
+
+get '/spending-tracker/tags' do #index
+  @tags = Tag.all
+  erb(:"tags/index")
+end
