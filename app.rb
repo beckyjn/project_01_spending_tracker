b2c52f0b4c96865198920d15cf658419d68fe39d
Becky <rebeccajoynielsen@gmail.come>
2019-03-26 16:08:59 +0000

    view budget on home page

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require_relative('./controllers/merchants_controller')
 require_relative('./controllers/tags_controller')
 require_relative('./controllers/accounts_controller')
 
+require_relative('./models/account.rb')
+
 get '/spending-tracker/' do
+    accounts = Account.all
+    @account = accounts.first
   erb(:index)
 end
